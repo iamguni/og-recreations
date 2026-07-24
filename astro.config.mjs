220c4801f3c09136a13c6bfa910d813284c4f767
iamguni <gurmeetworks@gmail.com>
2026-07-24 10:01:00 +0530

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,6 @@ import tailwind from '@astrojs/tailwind';
 
 export default defineConfig({
   integrations: [tailwind()],
-  site: 'https://iamguni.in',
-  base: '/og-recreations',
+  site: 'https://ogrecreations.com',
+  base: '',
 });
